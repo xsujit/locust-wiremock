0ca17f5fb046c934fcd42f0e5bc04bb7885c3f31
Sujit <xsujit@gmail.com>
2020-09-20 23:35:36 +0100

    Updated to locust 1.2.3

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 
 # Prerequisite - Wiremock
 Download wiremock standalone jar and run it
-E.g. java -jar wiremock-jre8-standalone-2.26.3.jar --https-port 8443
+E.g. java -jar wiremock-standalone-2.27.2.jar
+For https add --https-port 8443
 
-# Locust (v0.14.5) performance test against wire mock instance
+# Locust (v1.2.3) performance test against wire mock instance
 Use following command to run the test
-locust -f src/test/locust_test.py --no-web -c 1 -r 1 --run-time 1m
-OR 
-locust -f src/test/locust_test.py --csv=example --no-web -c 1 -r 1 --run-time 1m
+locust -f locust_test.py  --headless -u 10 -r 1 --run-time 30s
+The test will delete any existing wiremock mappings.
+It will create new mappings and the run the tests against those endpoints 
+
+# Remove all stopped containers and images
+docker rm $(docker ps -a -q)
+docker image prune
+# Build
+docker build -t locustio/locust:wm-locust .
+# Run
+docker run --network="host" locustio/locust:wm-locust -f /src/test/locust_test.py --headless -u 1 -r 1 -t 1m
+
+# Other docker commands
+docker run -p 8089:8089 --network="host" -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/locust_test.py --headless -u 1 -r 1 -t 1m
--- src/test/locust_test.py
+++ src/test/locust_test.py
@@ -1,87 +1,58 @@
-from locust import HttpLocust, TaskSet, task, between
-import urllib3
-import random
 import json
+import logging
+import random
 import requests
+from locust import HttpUser, task, between, events, SequentialTaskSet
 
 
-class UserBehavior(TaskSet):
-    id_dict = {}  # create a pool of ids from which to GET
-    counter = 0
+class UserBehavior(SequentialTaskSet):
 
-    def on_start(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.doc_id = None
+        self.map_id = None
 
-    @staticmethod
-    def on_stop():
-        pass
-
-    @task(3)
-    def get_document(self):
-        max_len = len(self.id_dict) if len(self.id_dict) <= 5 else 5
-        random_value = random.randint(1, max_len)
-        print("Len: {} | Random: {}".format(len(self.id_dict), random_value))
-        doc_id = str(self.id_dict[random_value]["id"])
-        url_path = "/document/" + doc_id
-        print("Url: " + url_path)
-        r = self.client.get(url_path, verify=False)
-        assert r.status_code == 200
-
-    @task(1)
+    @task
     def create_document_task(self):
-        doc_id = self.get_random()
-        self.counter += 1
-        new_stub = "/document/" + str(doc_id)
+        self.doc_id = random.randint(100, 999)
+        new_stub = f"/document/{self.doc_id}"
         stub_payload = json.dumps({"request": {"url": new_stub, "method": "GET"}, "response": {"status": 200}})
-        wire_mock_url = "http://localhost:8080/__admin/mappings/new"
-        r = self.client.post(wire_mock_url, data=stub_payload, headers={"content-type": "application/json"},
-                             verify=False)
+        headers = {"content-type": "application/json"}
+        r = self.client.post("/__admin/mappings", data=stub_payload, headers=headers)
+        self.map_id = json.loads(r.text)["id"]
         assert r.status_code == 201
 
-    def get_random(self):
-        while True:
-            print("Inside while")
-            found = False
-            doc_id = random.randint(1, 999999999)
-            for x in self.id_dict.values():
-                if doc_id == x["id"]:
-                    found = True
-                    break
-            if not found:
-                break
-        return doc_id
-
-
-class WebsiteUser(HttpLocust):
-    counter = 0
-
-    def setup(self):
-        print("Setup called")
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        r = requests.delete("http://localhost:8080/__admin/mappings")
+    @task
+    def get_document(self):
+        r = self.client.get(f"/document/{self.doc_id}", name="/document/id")
         assert r.status_code == 200
-        r = requests.delete("http://localhost:8080/__admin/requests")
+
+    @task
+    def delete_document(self):
+        r = self.client.delete(f"/__admin/mappings/{self.map_id}", name="/__admin/mappings/id")
         assert r.status_code == 200
-        for i in range(0, 500):
-            doc_id = i
-            sample_data = {"id": doc_id}
-            self.counter += 1
-            nest_data = {self.counter: sample_data}
-            new_stub = "/document/" + str(doc_id)
-            stub_payload = json.dumps({"request": {"url": new_stub, "method": "GET"}, "response": {"status": 200}})
-            wire_mock_url = "http://localhost:8080/__admin/mappings/new"
-            r = requests.post(wire_mock_url, data=stub_payload, headers={"content-type": "application/json"})
-            assert r.status_code == 201
-            UserBehavior.id_dict.update(nest_data)
+
+
+class WebsiteUser(HttpUser):
 
     @staticmethod
-    def teardown():
-        print("Teardown  called")
+    @events.test_start.add_listener
+    def setup(environment, **kwargs):
+        logging.info("Setup called")
         r = requests.delete("http://localhost:8080/__admin/mappings")
         assert r.status_code == 200
         r = requests.delete("http://localhost:8080/__admin/requests")
         assert r.status_code == 200
 
-    host = "https://localhost:8443"
-    task_set = UserBehavior
-    wait_time = between(10.0, 10.0)
+    @staticmethod
+    @events.test_stop.add_listener
+    def teardown(environment, **kwargs):
+        logging.info("Teardown  called")
+        r = requests.delete("http://localhost:8080/__admin/mappings")
+        assert r.status_code == 200
+        r = requests.delete("http://localhost:8080/__admin/requests")
+        assert r.status_code == 200
+
+    host = "http://localhost:8080"
+    tasks = [UserBehavior]
+    wait_time = between(1, 1)
